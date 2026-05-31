01900ba191ff49588ab36434e5943dc1b17161f0
nlkcodenew <ngoclinhmyhuong@gmail.com>
2026-05-31 14:45:48 +0700

    perf: make telemetry intervals env-driven and reduce HA poll latency

--- custom_components/lumentreelocal/const.py
+++ custom_components/lumentreelocal/const.py
@@ -22,6 +22,6 @@ SERVICE_SET_DISCHARGE_TIME_START = "set_discharge_time_start"
 SERVICE_SET_DISCHARGE_TIME_END = "set_discharge_time_end"
 
 DEFAULT_API_URL = "https://lumentree.jonah.io.vn"
-DEFAULT_SCAN_INTERVAL = timedelta(seconds=10)
+DEFAULT_SCAN_INTERVAL = timedelta(seconds=5)
 FAST_COMMAND_SCAN_INTERVAL = timedelta(seconds=2)
 SWITCH_WRITE_CONFIRM_TIMEOUT_SECONDS = 25
